f004ca8bd20aeea47fdf6c900a6e694b0a3ebe88
wood3n <wangkka1@163.com>
2024-05-07 17:14:48 +0800

    feat: 修改rsbuild配置

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+	plugins: [pluginReact(), pluginSvgr()],
+	dev: {
+		writeToDisk: true,
+	},
+	server: {
+		publicDir: {
+			watch: true,
+		},
+	},
 });
