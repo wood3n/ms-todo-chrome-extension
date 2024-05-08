a14fc0104aa0c2b95202cc376c94b7c1ea7e477a
wood3n <wangkka1@163.com>
2024-05-08 23:00:22 +0800

    feat: 拆分打包入口

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,22 +3,44 @@ import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default defineConfig({
-	plugins: [pluginReact(), pluginSvgr()],
-	output: {
-		copy:
-			process.env.NODE_ENV === "development"
-				? [{ from: "./public", to: "" }]
-				: undefined,
-	},
-	dev: {
-		writeToDisk: true,
-	},
-	server: {
-		publicDir: {
-			watch: true,
-		},
-	},
-	html: {
-		template: "./template/index.html",
-	},
+  plugins: [pluginReact(), pluginSvgr()],
+  source: {
+    // 当 entry 为 index 时，可通过 / 访问页面；当 entry 为 option 时，可通过 /option 访问该页面
+    entry: {
+      popup: "./src/entries/popup/index.tsx",
+      option: "./src/entries/option/index.tsx",
+      background: {
+        import: "./src/entries/popup/background.ts",
+        filename: "background.js",
+      },
+    },
+  },
+  output: {
+    copy:
+      process.env.NODE_ENV === "development"
+        ? [{ from: "./public", to: "" }]
+        : undefined,
+  },
+  dev: {
+    writeToDisk: true,
+  },
+  server: {
+    publicDir: {
+      watch: true,
+    },
+  },
+  html: {
+    template({ entryName }) {
+      const templates = {
+        popup: "./src/entries/popup/index.html",
+        option: "./src/entries/option/index.html",
+      };
+      return templates[entryName];
+    },
+  },
+  performance: {
+    chunkSplit: {
+      strategy: "all-in-one",
+    },
+  },
 });
--- src/entries/popup/index.tsx
+++ src/entries/popup/index.tsx
@@ -3,15 +3,15 @@ import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { msalInstance } from "./auth/msalInstance";
+import { msalInstance } from "../../auth/msalInstance";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-	<React.StrictMode>
-		<NextUIProvider>
-			<MsalProvider instance={msalInstance}>
-				<App />
-			</MsalProvider>
-		</NextUIProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <NextUIProvider>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </NextUIProvider>
+  </React.StrictMode>
 );
