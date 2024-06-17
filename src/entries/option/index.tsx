d21719c16ac6ca7eab0701f18b94ac8e3208415c
wangkai.1997 <wangkka1@163.com>
2024-06-17 10:30:13 +0800

    fix: rename file

--- src/entries/option/index.tsx
+++ src/entries/option/index.tsx
@@ -1,7 +1,7 @@
 import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
