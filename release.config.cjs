515f5bc3a29a5c8e3e41884fbe648397eba5e1df
wood3n <wangkka1@163.com>
2024-12-19 20:36:11 +0800

    chore: fix github actions

--- release.config.cjs
+++ release.config.cjs
@@ -6,10 +6,11 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     [
       "@semantic-release/github",
       {
-        assets: ["dist/**"],
+        assets: ["extension.zip"],
       },
     ],
   ],
